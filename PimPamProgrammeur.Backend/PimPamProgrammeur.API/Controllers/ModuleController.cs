69fcf4e8b7e5f097e8638e4fa4d97dac064c9b9f
Jan-wiebe Glasbergen <17081246@student.hhs.nl>
2020-10-27 17:37:29 +0100

    Fixed issue with DI Added swashbuckle for autogenerating swagger docs Added moduleReqeust validator

--- PimPamProgrammeur.Backend/PimPamProgrammeur.API/Controllers/ModuleController.cs
+++ PimPamProgrammeur.Backend/PimPamProgrammeur.API/Controllers/ModuleController.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PimPamProgrammeur.API.Processors;
 using PimPamProgrammeur.Dto;
+using PimPamProgrammeur.Dto.Validator;
+
 
 namespace PimPamProgrammeur.API.Controllers
 {
@@ -15,13 +14,30 @@ namespace PimPamProgrammeur.API.Controllers
     public class ModuleController : ControllerBase
     {
         private readonly IModuleProcessor _processor;
-        public ModuleController(IModuleProcessor processor)
+        private readonly IValidator<ModuleRequestDto> _moduleRequestValidator;
+
+        public ModuleController(IModuleProcessor processor, IValidator<ModuleRequestDto> moduleRequestValidator)
         {
             _processor = processor;
+            _moduleRequestValidator = moduleRequestValidator;
         }
+
+        /// <summary>
+        /// Saves a module to the database
+        /// </summary>
+        /// <param name="request">The module to save</param>
+        /// <returns>The saved module</returns>
         [HttpPost]
+        [ProducesResponseType(typeof(ModuleResponseDto), 200)]
+        [ProducesResponseType(typeof(ValidationResult), 400)]
         public async Task<IActionResult> PostModule(ModuleRequestDto request)
         {
+            var validationResult = _moduleRequestValidator.Validate(request);
+            if (validationResult.Errors.Any())
+            {
+                return BadRequest(validationResult);
+            }
+
             var module = await _processor.SaveModule(request);
 
             return Ok(module);
--- PimPamProgrammeur.Backend/PimPamProgrammeur.API/Startup.cs
+++ PimPamProgrammeur.Backend/PimPamProgrammeur.API/Startup.cs
@@ -1,20 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using PimPamProgrammeur.API.Mapping;
 using PimPamProgrammeur.API.Processors;
 using PimPamProgrammeur.Data;
+using PimPamProgrammeur.Dto;
+using PimPamProgrammeur.Dto.Validator;
 using PimPamProgrammeur.Repository;
 
 namespace PimPamProgrammeur.API
@@ -35,28 +30,39 @@ namespace PimPamProgrammeur.API
            opt.UseSqlServer(Configuration.GetConnectionString("PimPamProgrammeurConnection"))
            .EnableSensitiveDataLogging());
 
-            // repositories
-            services.AddSingleton<IModuleRepository, ModuleRepository>();
+            // Repositories
+            services.AddTransient<IModuleRepository, ModuleRepository>();
 
-            // automapper
+            // AutoMapper
             var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new DtoMapping()); });
             IMapper mapper = mapperConfig.CreateMapper();
             services.AddSingleton(mapper);
 
             // Processor
-            services.AddSingleton<IModuleProcessor, ModuleProcessor>();
+            services.AddTransient<IModuleProcessor, ModuleProcessor>();
+
+            // Validators
+            services.AddSingleton<IValidator<ModuleRequestDto>, ModuleRequestDtoValidator>();
 
             // controllers
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
 
             app.UseHttpsRedirection();
 
