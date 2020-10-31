97963bb67cde0cf4aecc67b39db216aac88bb53b
Lars Tetteroo <l.tetteroo@docspro.nl>
2020-10-31 11:34:13 +0100

    Add PasswordGeneratorService interface and implementation.

---
+++ PimPamProgrammeur.Backend/PimPamProgrammeur.Utils/PasswordGeneratorService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PimPamProgrammeur.Utils
+{
+    public class PasswordGeneratorService : IPasswordGeneratorService
+    {
+        private const string Characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+        public string Generate(int length)
+        {
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                byte[] data = new byte[length];
+
+                // If chars.Length isn't a power of 2 then there is a bias if we simply use the modulus operator. The first characters of chars will be more probable than the last ones.
+                // buffer used if we encounter an unusable random byte. We will regenerate it in this buffer
+                byte[] buffer = null;
+
+                // Maximum random number that can be used without introducing a bias
+                int maxRandom = byte.MaxValue - ((byte.MaxValue + 1) % Characters.Length);
+
+                crypto.GetBytes(data);
+
+                char[] result = new char[length];
+
+                for (int i = 0; i < length; i++)
+                {
+                    byte value = data[i];
+
+                    while (value > maxRandom)
+                    {
+                        if (buffer == null)
+                        {
+                            buffer = new byte[1];
+                        }
+
+                        crypto.GetBytes(buffer);
+                        value = buffer[0];
+                    }
+
+                    result[i] = Characters[value % Characters.Length];
+                }
+                return new string(result);
+            }
+        }
+    }
+}
