279a82c169047fdf27d334c6ab3714bb6e8d6156
Lars Tetteroo <l.tetteroo@docspro.nl>
2020-11-03 20:55:46 +0100

    Fix password hashing. Add VerifyHashedPassword method.

--- PimPamProgrammeur.Backend/PimPamProgrammeur.Utils/HashingService.cs
+++ PimPamProgrammeur.Backend/PimPamProgrammeur.Utils/HashingService.cs
@@ -9,9 +9,14 @@ namespace PimPamProgrammeur.Utils
     {
         private const string pepper = @";g!dCzSOX*{xk1kDLt&//K<p,hjJI&\tw%PKcgdv\bLNrD!O8eQCG;l*x%\`Snlv\g$h5_u3Vn ~%0-kEZ[= aPjY & V > u &)`Dmy#BN:<IbHN-cDs]-;tDK:<Jw1>V*+J%";
 
-        public string HashPassword(string email, string plainTextPassword)
+        public string HashPassword(string plainTextPassword)
         {
-            return Crypto.HashPassword(email + plainTextPassword + pepper);
+            return Crypto.HashPassword(plainTextPassword + pepper);
+        }
+
+        public bool VerifyHashedPassword(string hashedPassword, string plainTextPassword)
+        {
+            return Crypto.VerifyHashedPassword(hashedPassword, plainTextPassword + pepper);
         }
     }
 }
