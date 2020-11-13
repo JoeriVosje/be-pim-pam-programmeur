b4f8bff525ae98733e229c4a2c8b849aeef240f5
Jan-wiebe Glasbergen <17081246@student.hhs.nl>
2020-11-13 11:52:35 +0100

    Added using in tokenProvider

--- PimPamProgrammeur.Backend/PimPamProgrammeur.Utils/TokenProvider.cs
+++ PimPamProgrammeur.Backend/PimPamProgrammeur.Utils/TokenProvider.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
