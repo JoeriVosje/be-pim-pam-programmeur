a64c3421581cfdcc7bc863b4df755f53f8ff646a
Jan-wiebe Glasbergen <17081246@student.hhs.nl>
2020-11-14 18:05:54 +0100

    Postman fixes

--- PimPamProgrammeur.Backend/PimPamProgrammeur.Dto/Validator/ComponentRequestDtoValidator.cs
+++ PimPamProgrammeur.Backend/PimPamProgrammeur.Dto/Validator/ComponentRequestDtoValidator.cs
@@ -14,9 +14,14 @@ namespace PimPamProgrammeur.Dto.Validator
             ValidateNullOrEmpty(validationResult, nameof(entity.Title), entity.Title);
             ValidateNullOrEmpty(validationResult, nameof(entity.Theory), entity.Theory);
             ValidateNullOrEmpty(validationResult, nameof(entity.Question), entity.Question);
-            ValidateNullOrEmpty(validationResult, nameof(entity.Skippable), entity.Skippable.ToString());
+            ValidateNull(validationResult, nameof(entity.Skippable), entity.Skippable);
             ValidateNullOrEmpty(validationResult, nameof(entity.Hint), entity.Hint);
-            ValidateNullOrEmpty(validationResult, nameof(entity.ModuleId), entity.ModuleId.ToString());
+            ValidateNull(validationResult, nameof(entity.ModuleId), entity.ModuleId);
+            if (!entity.Skippable && entity.Answers.Count == 0)
+            {
+                validationResult.Errors.Add("A question must be skippable when there no answers provided");
+            }
+
 
             return validationResult;
         }
